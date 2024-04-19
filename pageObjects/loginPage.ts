2356ddd4996f767d3aab34975f321af3888e8497
Loboyko Ilya <loboykois@gmail.com>
2024-04-19 14:55:17 +0300

    test(ArtOrange): add resource link to LoginPage class

--- pageObjects/loginPage.ts
+++ pageObjects/loginPage.ts
@@ -4,7 +4,7 @@ export class LoginPage {
   public constructor(private readonly page: Page) {}
 
   public async navigate(): Promise<void> {
-    await this.page.goto("/");
+    await this.page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
   }
 
   public async doLogin(): Promise<void> {
